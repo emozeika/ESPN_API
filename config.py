9c10d07ea88d5c6f7fb428fb6222f3ce91accd38
Eric Mozeika <mozeikaeric@gmail.com>
2022-03-31 23:18:10 -0400

    Adding config file to hold import variables

---
+++ config.py
@@ -0,0 +1,8 @@
+#URL structure variables for ESPN API
+COREURL = 'http://sports.core.api.espn.com/'
+BASEURL = 'https://site.api.espn.com/apis/site/'
+VERSIONURL = 'v2/'
+
+
+#Mongo DB local cluster
+LOCAL_CLUSTER = "mongodb//localhost/27017/S"
