e1c6c42aec97284042839c327318dbc796741a3e
emozeika <mozeikaeric@gmail.com>
2022-05-07 22:34:04 -0400

    created function to pull historical data for a list of dates.

---
+++ baseball_extract.py
@@ -0,0 +1,29 @@
+from boxscore import BoxScoreExtract
+from datetime import datetime, timedelta
+
+from scoreboard import ScoreBoardExtract
+
+def pull_historical_data(league, start_date, end_date = None):
+    '''
+    Function to pull historical data based on dates. start_date and
+    end_date should be in format YYYYMMDD
+    '''
+    #creating datetime values
+    current_date = datetime.strptime(start_date, '%Y%m%d')
+    if end_date:
+        end_date = datetime.strptime(end_date, '%Y%m%d')
+    else:
+        end_date = datetime.today()
+
+    #loop to run through dates
+    while current_date < end_date:
+        ScoreBoardExtract(league = league).save_scoreboard(date = current_date.strftime('%Y%m%d'))
+        BoxScoreExtract(league = league).save_boxscores(date = current_date.strftime('%Y%m%d'))
+
+        current_date += timedelta(days = 1)
+
+    
+
+pull_historical_data('mlb', '20220105')
+
+
