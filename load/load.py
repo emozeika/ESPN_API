12482fb7b946c7c3adb51f778adfc4f0b6ed3011
emozeika <mozeikaeric@gmail.com>
2022-05-17 21:34:00 -0400

    began load class.

---
+++ load/load.py
@@ -0,0 +1,33 @@
+import psycopg2
+import os
+import sys
+
+
+#loading config variables from parent directory
+base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(base_dir)
+import config
+
+class PostgresClient:
+    '''
+    Client to send csv data to postgres database
+    '''
+    HOSTNAME = config.POSTGRES_HOSTNAME
+    USERNAME = config.POSTGRES_USERNAME
+    PASSWORD = config.POSTGRES_PWD
+    PORT = config.POSTGRES_PORT
+
+    def __init__(self, database):
+        self.DATABASE = database
+        self.CONNECTION = psycopg2.connect(
+                                        host = self.HOSTNAME,
+                                        dbname = self.DATABASE,
+                                        user = self.USERNAME,
+                                        password = self.PASSWORD,
+                                        port = self.PORT
+        )
+
+    def close_connection(self):
+        self.CONNECTION.close()
+
+
