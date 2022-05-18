8d12be97c3a8c829b25d2444233d15a59f6480af
emozeika <mozeikaeric@gmail.com>
2022-05-18 18:33:17 -0400

    created pitching and fielding stats functions.

--- transform/baseballstats.py
+++ transform/baseballstats.py
@@ -23,7 +23,8 @@ class BaseballStats(Transform):
 
     TABLE_MAPPING = {
         'batting' : 'batting_stats.csv',
-        'pitching' : 'pitching_stats.csv'
+        'pitching' : 'pitching_stats.csv',
+        'fielding' : 'fielding_stats.csv'
     }
 
     DTYPE_MAPPING = {
@@ -50,11 +51,12 @@ class BaseballStats(Transform):
 
     def create_batting_table(self, file_name):
         '''
-        This is a function to create the table for the stats for an event and save it to a csv.
+        This is a function to create the table for the battingstats for an event and save it to a csv.
         
         -------------------------------
         event_id (int)              id from ESPN associated to the specific event
         team_id (int)               id that regards to the current teams stats
+        event_team_id(int)          compound key for event and team stats
         hitByPitch (int)            the amount times a batter was hit by pitch 
         groundBalls (int)           the amount of outs that came by ground balls
         strikeouts (int)            the amount of strikeouts in the game
@@ -90,7 +92,9 @@ class BaseballStats(Transform):
             'event_team_id' : []
         }
 
-        raw_data = self.open_file(file_name)
+        file = open(self.RAW_DATA_PATH + file_name)
+        raw_data = json.load(file)
+        file.close()
 
         #adding event id to dict twice for each team row
         event_data['event_id'] = [raw_data['header']['id'] for i in range(2)]
@@ -123,15 +127,165 @@ class BaseballStats(Transform):
                     else:
                         event_data[stat].append(raw_data['boxscore']['teams'][key]['statistics'][batting_stats_key]['stats'][stat_key]['value'])
 
+        event_data = pd.DataFrame(event_data)
+        return event_data
+
+    
+    def create_pitching_table(self, file_name):
+        '''
+        This is a function to create the table for the pitching stats for an event and save it to a csv.
+        
+        -------------------------------
+        event_id (int)              id from ESPN associated to the specific event
+        team_id (int)               id that regards to the current teams stats
+        event_team_id(int)          compound key for event and team stats
+        save (int)                  if there was a save in the game
+        groundBalls (int)           number of outs recorded by ground ball
+        strikeouts (int)            number of outs recorded by strikeouts
+        sacFlies (int)              number of sacrifice flies given up 
+        hits (int)                  number of hits given up in the game
+        earnedRuns (int)            number of earned runs given up in the game
+        battersHit (int)            number of batters hit in the game
+        walks (int)                 number of base on balls in the game
+        runs (int)                  number of runs given up in the game
+        stolenBases (int)           number of stolen bases given up in the game
+        caughtStealing (int)        number of outs recorded by steals in a game
+        homeRuns (int)              number of home runs given up in the game
+        triples (int)               number of triples given up in a game
+        balks (int)                 number of balks in a game
+        flyBalls (int)              number of flyballs produced in the game
+        battersFaced (int)          number of batters faced in a game
+        holds (int)                 number of holds in a game
+        doubles (int)               number of doubles given up in a game
+        wildPitches (int)           number of wild pitches thrown in a game
+        atBats (int)                number of at bats in the game
+        opponentTotalBases          number of total bases given up in the game
+        blownSaves (int)            number of blown saves in the game
+        ERA (float)                 earned run average for the game
+        WHIP (float)                walks plus hits per inning for the game
+        groundToFlyRatio (float)    ratio of ground balls to fly balls for the game
+        opponentAvg (float)         opponents batting average for the game
+        opponentSlugAvg (float)     opponents slugging percentage for the game
+        opponentOnBasePct (float)   opponents on base percentage for the game
+        opponentOPS (float)         opponents SLUG + OBP for the game
+        strikeoutsPerNineInnings (float) number of strikesouts per game
+        strikeoutToWalkRatio (float) strikes to walk ratio for the game
+        BIPA (float)                batting avg on balls in play for the game
+        '''
+
+        event_data = {
+            'event_id' : [],
+            'team_id' : [],
+            'event_team_id' : []
+        }
+
+        file = open(self.RAW_DATA_PATH + file_name)
+        raw_data = json.load(file)
+        file.close()
+
+        #adding event id to dict twice for each team row
+        event_data['event_id'] = [raw_data['header']['id'] for i in range(2)]
+
+        #abbrv of batting stat names 
+        pitching_stats = [
+            'save', 'groundBalls', 'strikeouts', 'sacFlies', 'hits', 'earnedRuns', 'battersHit',
+            'walks', 'runs', 'stolenBases', 'caughtStealing', 'homeRuns', 'triples', 'balks', 
+            'flyBalls', 'battersFaced', 'holds', 'doubles', 'wildPitches', 'atBats', 'opponentTotalBases',
+            'blownSaves', 'ERA', 'WHIP', 'groundToFlyRatio', 'opponentAvg', 'opponentSlugAvg', 
+            'opponentOnBasePct', 'opponentOPS', 'strikeoutsPerNineInnings', 
+            'strikeoutToWalkRatio', 'BIPA' 
+        ]
+
+        #initalize empty list for each stat into dict
+        for stat in pitching_stats:
+            event_data[stat] = []
+
+        #grabbing stats for each team in statistics tab
+        for key in raw_data['boxscore']['teams']:
+            event_data['team_id'].append(raw_data['boxscore']['teams'][key]['team']['id'])
+            event_data['event_team_id'].append(event_data['event_id'][0] + '~' + raw_data['boxscore']['teams'][key]['team']['id'])
+            pitching_stats_key = self.find_key(raw_data['boxscore']['teams'][key]['statistics'], 'pitching')
+            
+            for stat in pitching_stats:
+                if pitching_stats_key is None:
+                    event_data[stat].append(None)
+                else:
+                    stat_key = self.find_key(raw_data['boxscore']['teams'][key]['statistics'][pitching_stats_key]['stats'], stat)
+                    if stat_key is None:
+                        event_data[stat].append(None)
+                    else:
+                        event_data[stat].append(raw_data['boxscore']['teams'][key]['statistics'][pitching_stats_key]['stats'][stat_key]['value'])
+
 
 
         event_data = pd.DataFrame(event_data)
         return event_data
 
     
-    #TODO: create pitching stats table function
-    def create_pitching_table(self, file_name):
-        pass
+    def create_fielding_table(self, file_name):
+        '''
+        This is a function to create the table for the fielding stats for an event and save it to a csv.
+        
+        -------------------------------
+        event_id (int)                      id from ESPN associated to the specific event
+        team_id (int)                       id that regards to the current teams stats
+        event_team_id(int)                  compound key for event and team stats
+        doublePlays (int)                   amount of double plays by defense in a game 
+        errors (int)                        amount of errors by defense in a game
+        passedBalls (int)                   amount of passed balls by catcher in a game
+        assists (int)                       amount of assists by defense in a game
+        outfieldAssists (int)               amount of assists by outfield in a game
+        pickoffs (int)                      amount of successful pickoffs in a game
+        catcherCaughtStealing (int)         number of base runners thrown out in a game
+        catcherStolenBasesAllowed (int)     number of stolen bases in a game
+        fieldingPct (int)                   fielding percentage for the game
+        rangeFactor (float)                 avergae range for fielders in a game
+        zoneRating (float)                  average ability of a fielder to defend a zone in the game
+        catcherCaughtStealingPct (float)    catchers caught stealing percentage for the game
+        '''
+
+        event_data = {
+            'event_id' : [],
+            'team_id' : [],
+            'event_team_id' : []
+        }
+
+        file = open(self.RAW_DATA_PATH + file_name)
+        raw_data = json.load(file)
+        file.close()
+
+        #adding event id to dict twice for each team row
+        event_data['event_id'] = [raw_data['header']['id'] for i in range(2)]
+
+        #abbrv of batting stat names 
+        fielding_stats = [
+            'doublePlays', 'errors', 'passedBalls', 'assists', 'outfieldAssists', 'pickoffs',
+            'catcherCaughtStealing', 'catcherStolenBasesAllowed', 'fieldingPct', 'rangeFactor',
+            'zoneRating', 'catcherCaughtStealingPct'            
+        ]
+
+        #initalize empty list for each stat into dict
+        for stat in fielding_stats:
+            event_data[stat] = []
+
+        #grabbing stats for each team in statistics tab
+        for key in raw_data['boxscore']['teams']:
+            event_data['team_id'].append(raw_data['boxscore']['teams'][key]['team']['id'])
+            event_data['event_team_id'].append(event_data['event_id'][0] + '~' + raw_data['boxscore']['teams'][key]['team']['id'])
+            fielding_stats_key = self.find_key(raw_data['boxscore']['teams'][key]['statistics'], 'fielding')
+            
+            for stat in fielding_stats:
+                if fielding_stats_key is None:
+                    event_data[stat].append(None)
+                else:
+                    stat_key = self.find_key(raw_data['boxscore']['teams'][key]['statistics'][fielding_stats_key]['stats'], stat)
+                    if stat_key is None:
+                        event_data[stat].append(None)
+                    else:
+                        event_data[stat].append(raw_data['boxscore']['teams'][key]['statistics'][fielding_stats_key]['stats'][stat_key]['value'])
+
+        event_data = pd.DataFrame(event_data)
+        return event_data
 
 
     def concat_tables(self, table, new_table, id_field = None):
@@ -147,6 +301,8 @@ class BaseballStats(Transform):
             return self.create_batting_table(file_name)
         elif table_name == 'pitching':
             return self.create_pitching_table(file_name)
+        elif table_name == 'fielding':
+            return self.create_fielding_table(file_name)
 
 
     def update_table(self, file_names, table_name, id_field = None):
@@ -179,4 +335,4 @@ class BaseballStats(Transform):
 
 
 if __name__ == '__main__':
-    BaseballStats().update_table(['401423754.json', '401431516.json', '320410111.json'], 'batting', 'event_team_id')
+    BaseballStats().update_table(['401423754.json', '401431516.json', '320410111.json'], 'fielding', 'event_team_id')
