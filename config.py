ce02b54cc077316ff82b7b70238e502bbf5aec37
emozeika <mozeikaeric@gmail.com>
2022-05-18 18:28:33 -0400

    added data type dicts to config.

--- config.py
+++ config.py
@@ -80,3 +80,61 @@ batting_stats_dtypes = {
     'BIPA' : 'float64', 
     'offWARBR' : 'float64'
 }
+
+#stats/boxscore
+pitching_stats_dtypes = {
+    'event_id' : 'Int64',
+    'team_id' : 'Int64',
+    'event_team_id' : 'string',
+    'save' : 'Int64', 
+    'groundBalls' : 'Int64', 
+    'strikeouts' : 'Int64', 
+    'sacFlies' : 'Int64', 
+    'hits' : 'Int64', 
+    'earnedRuns' : 'Int64', 
+    'battersHit' : 'Int64',
+    'walks' : 'Int64', 
+    'runs' : 'Int64', 
+    'stolenBases' : 'Int64', 
+    'caughtStealing' : 'Int64', 
+    'homeRuns' : 'Int64', 
+    'triples' : 'Int64', 
+    'balks' : 'Int64', 
+    'flyBalls' : 'Int64', 
+    'battersFaced' : 'Int64', 
+    'holds' : 'Int64', 
+    'doubles' : 'Int64', 
+    'wildPitches' : 'Int64', 
+    'atBats' : 'Int64', 
+    'opponentTotalBases' : 'Int64',
+    'blownSaves' : 'Int64', 
+    'ERA' : 'float64', 
+    'WHIP' : 'float64', 
+    'groundToFlyRatio' : 'float64', 
+    'opponentAvg' : 'float64', 
+    'opponentSlugAvg' : 'float64',
+    'opponentOnBasePct' : 'float64', 
+    'opponentOPS' : 'float64', 
+    'strikeoutsPerNineInnings' : 'float64', 
+    'strikeoutToWalkRatio' : 'float64', 
+    'BIPA' : 'float64' 
+}
+
+#stats/boxscore
+pitching_stats_dtypes = {
+    'event_id' : 'Int64',
+    'team_id' : 'Int64',
+    'event_team_id' : 'string',
+    'doublePlays' : 'Int64', 
+    'errors' : 'Int64', 
+    'passedBalls' : 'Int64', 
+    'assists' : 'Int64', 
+    'outfieldAssists' : 'Int64', 
+    'pickoffs' : 'Int64',
+    'catcherCaughtStealing' : 'Int64', 
+    'catcherStolenBasesAllowed' : 'Int64', 
+    'fieldingPct' : 'float64', 
+    'rangeFactor' : 'float64',
+    'zoneRating' : 'float64', 
+    'catcherCaughtStealingPct' : 'float64'
+}
