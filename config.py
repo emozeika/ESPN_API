7dc16ebc5b82fa0cf0243c0fed836b01f5e967da
emozeika <mozeikaeric@gmail.com>
2022-05-18 22:08:40 -0400

    adding mapping dicts

--- config.py
+++ config.py
@@ -9,19 +9,24 @@ VERSIONURL = 'v2/'
 
 
 #================================================================================
-# POSTGRES CLIENT INFO
+# POSTGRES CLIENT INFO AND MAPPINGS
 #================================================================================
 
 POSTGRES_HOSTNAME = 'localhost'
 POSTGRES_USERNAME = 'postgres'
 POSTGRES_PWD = 'Emoz3832!'
 POSTGRES_PORT = 5432
-
+POSTGRES_DATABASE = 'postgres'
 
 POSTGRES_MLB_DB = 'mlb'
 POSTGRES_NBA_DB = 'nba'
 
-
+dtype_mappings = {
+    'int64' : 'INT',
+    'string' : 'VARCHAR(99)',
+    'float64' : 'DOUBLE',
+    'bool' : 'BOOL' 
+}
 
 
 #Mongo DB local cluster
@@ -34,43 +39,43 @@ LOCAL_CLUSTER = "mongodb://localhost:27017/"
 
 #scoreboard
 scoreboard_dtypes = {
-    'event_id' : 'Int64',
+    'event_id' : 'int64',
     'event_date' : 'string',
-    'home_team_id' : 'Int64',
-    'away_team_id' : 'Int64',
+    'home_team_id' : 'int64',
+    'away_team_id' : 'int64',
     'event_site_name' : 'string',
     'neutral_site' : 'bool',
-    'home_team_score' : 'Int64',
-    'away_team_score' : 'Int64',
-    'season_type_id' : 'Int64',
+    'home_team_score' : 'int64',
+    'away_team_score' : 'int64',
+    'season_type_id' : 'int64',
     'season_type_name' : 'string',
-    'season_year' : 'Int64'
+    'season_year' : 'int64'
 }
 
 #stats/boxscore
 batting_stats_dtypes = {
-    'event_id' : 'Int64',
-    'team_id' : 'Int64',
+    'event_id' : 'int64',
+    'team_id' : 'int64',
     'event_team_id' : 'string',
-    'hitByPitch' : 'Int64', 
-    'groundBalls' : 'Int64',
-    'strikeouts' : 'Int64', 
-    'RBIs' : 'Int64', 
-    'hits' : 'Int64', 
-    'stolenBases' : 'Int64', 
-    'walks' : 'Int64', 
-    'runs' : 'Int64', 
-    'atBats' : 'Int64', 
-    'sacFlies' : 'Int64', 
-    'homeRuns' : 'Int64', 
-    'runnersLeftOnBase' : 'Int64', 
-    'triples' : 'Int64', 
-    'doubles' : 'Int64', 
-    'flyBalls' : 'Int64', 
-    'caughtStealing' : 'Int64',
-    'totalBases' : 'Int64', 
-    'plateAppearances' : 'Int64', 
-    'extraBaseHits' : 'Int64', 
+    'hitByPitch' : 'int64', 
+    'groundBalls' : 'int64',
+    'strikeouts' : 'int64', 
+    'RBIs' : 'int64', 
+    'hits' : 'int64', 
+    'stolenBases' : 'int64', 
+    'walks' : 'int64', 
+    'runs' : 'int64', 
+    'atBats' : 'int64', 
+    'sacFlies' : 'int64', 
+    'homeRuns' : 'int64', 
+    'runnersLeftOnBase' : 'int64', 
+    'triples' : 'int64', 
+    'doubles' : 'int64', 
+    'flyBalls' : 'int64', 
+    'caughtStealing' : 'int64',
+    'totalBases' : 'int64', 
+    'plateAppearances' : 'int64', 
+    'extraBaseHits' : 'int64', 
     'avg' : 'float64', 
     'slugAvg' : 'float64', 
     'onBasePct' : 'float64', 
@@ -83,31 +88,31 @@ batting_stats_dtypes = {
 
 #stats/boxscore
 pitching_stats_dtypes = {
-    'event_id' : 'Int64',
-    'team_id' : 'Int64',
+    'event_id' : 'int64',
+    'team_id' : 'int64',
     'event_team_id' : 'string',
-    'save' : 'Int64', 
-    'groundBalls' : 'Int64', 
-    'strikeouts' : 'Int64', 
-    'sacFlies' : 'Int64', 
-    'hits' : 'Int64', 
-    'earnedRuns' : 'Int64', 
-    'battersHit' : 'Int64',
-    'walks' : 'Int64', 
-    'runs' : 'Int64', 
-    'stolenBases' : 'Int64', 
-    'caughtStealing' : 'Int64', 
-    'homeRuns' : 'Int64', 
-    'triples' : 'Int64', 
-    'balks' : 'Int64', 
-    'flyBalls' : 'Int64', 
-    'battersFaced' : 'Int64', 
-    'holds' : 'Int64', 
-    'doubles' : 'Int64', 
-    'wildPitches' : 'Int64', 
-    'atBats' : 'Int64', 
-    'opponentTotalBases' : 'Int64',
-    'blownSaves' : 'Int64', 
+    'save' : 'int64', 
+    'groundBalls' : 'int64', 
+    'strikeouts' : 'int64', 
+    'sacFlies' : 'int64', 
+    'hits' : 'int64', 
+    'earnedRuns' : 'int64', 
+    'battersHit' : 'int64',
+    'walks' : 'int64', 
+    'runs' : 'int64', 
+    'stolenBases' : 'int64', 
+    'caughtStealing' : 'int64', 
+    'homeRuns' : 'int64', 
+    'triples' : 'int64', 
+    'balks' : 'int64', 
+    'flyBalls' : 'int64', 
+    'battersFaced' : 'int64', 
+    'holds' : 'int64', 
+    'doubles' : 'int64', 
+    'wildPitches' : 'int64', 
+    'atBats' : 'int64', 
+    'opponentTotalBases' : 'int64',
+    'blownSaves' : 'int64', 
     'ERA' : 'float64', 
     'WHIP' : 'float64', 
     'groundToFlyRatio' : 'float64', 
@@ -122,17 +127,17 @@ pitching_stats_dtypes = {
 
 #stats/boxscore
 pitching_stats_dtypes = {
-    'event_id' : 'Int64',
-    'team_id' : 'Int64',
+    'event_id' : 'int64',
+    'team_id' : 'int64',
     'event_team_id' : 'string',
-    'doublePlays' : 'Int64', 
-    'errors' : 'Int64', 
-    'passedBalls' : 'Int64', 
-    'assists' : 'Int64', 
-    'outfieldAssists' : 'Int64', 
-    'pickoffs' : 'Int64',
-    'catcherCaughtStealing' : 'Int64', 
-    'catcherStolenBasesAllowed' : 'Int64', 
+    'doublePlays' : 'int64', 
+    'errors' : 'int64', 
+    'passedBalls' : 'int64', 
+    'assists' : 'int64', 
+    'outfieldAssists' : 'int64', 
+    'pickoffs' : 'int64',
+    'catcherCaughtStealing' : 'int64', 
+    'catcherStolenBasesAllowed' : 'int64', 
     'fieldingPct' : 'float64', 
     'rangeFactor' : 'float64',
     'zoneRating' : 'float64', 
